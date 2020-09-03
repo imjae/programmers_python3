fffd06baa993055746ecba615708ae370b346811
namu5 <jakeim11@namuplanet.com>
2020-09-03 17:33:15 +0900

    [UI전환]임재경(ERP): 레벨 디렉토리 추가 문제 추가

--- level1/행렬의덧셈.py
+++ level1/행렬의덧셈.py
@@ -1,13 +1,11 @@
 def solution(arr1, arr2):
-    array1 = []
-    
-
+    array1 = [[0]*len(arr1) for _ in arr1]
 
     for i1, v1 in enumerate(arr1):
         for i2, v2 in enumerate(v1):
             print(i1, i2)
-            answer[i1][i2] = arr1[i1][i2] + arr2[i1][i2]
+            array1[i1][i2] = arr1[i1][i2] + arr2[i1][i2]
 
-    return answer
+    return array1
 
 print(solution([[1,2],[2,3]], [[3,4],[5,6]]))
